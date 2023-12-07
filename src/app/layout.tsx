c7ef1c95242e46bbbaf770d36d42cf7c414ed952
Claire Chu(朱晏禛) <claire_chu@htc.com>
2023-12-07 13:12:11 +0800

    add basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-screen h-24 flex justify-center items-center">
+          <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">
+            Wordle
+          </h1>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
