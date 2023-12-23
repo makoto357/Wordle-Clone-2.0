e5717b8332d037f6796fd6e407f6acd2928a494a
Claire Chu(朱晏禛) <makoto357@gmail.com>
2023-12-23 23:14:33 +0800

    add transition effect to game status modal

--- src/components/GameStatusModal.tsx
+++ src/components/GameStatusModal.tsx
@@ -1,4 +1,5 @@
-import { Dialog } from '@headlessui/react';
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import restartGame from '../../public/restart-game.png';
 
@@ -16,27 +17,33 @@ const GameStatusModal: React.FC<GameStatusModalProps> = ({
   dialogDesc
 }) => {
   return (
-    <Dialog
-      className="relative z-50 text-black"
-      open={isOpen}
-      onClose={onClose}
+    <Transition
+      show={isOpen}
+      enter="transition duration-300 ease-out"
+      enterFrom="transform scale-95 opacity-0"
+      leave="transition duration-150 ease-out"
+      leaveFrom="transform scale-100 opacity-100"
+      as={Fragment}
     >
-      <div className="fixed inset-0 bg-white/50" aria-hidden="true" />
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-3">
-        <Dialog.Panel className="w-full max-w-sm rounded bg-macaron-blue p-6 text-center">
-          <Dialog.Title className="mb-3 text-lg font-bold">
-            {dialogTitle}
-          </Dialog.Title>
-          <Dialog.Description className="mb-3">{dialogDesc}</Dialog.Description>
-          <button
-            className="bg-white p-1 rounded-full text-center outline-none"
-            onClick={onClose}
-          >
-            <Image src={restartGame} alt="play again" width="30" />
-          </button>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+      <Dialog className="relative z-50 text-black" onClose={onClose}>
+        <div className="fixed inset-0 bg-white/50" aria-hidden="true" />
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-3">
+          <Dialog.Panel className="w-full max-w-sm rounded bg-macaron-blue p-6 text-center">
+            <Dialog.Title className="mb-3 text-lg font-bold">
+              {dialogTitle}
+            </Dialog.Title>
+            <Dialog.Description className="mb-3" />
+            {dialogDesc}
+            <button
+              className="bg-white p-1 rounded-full text-center outline-none"
+              onClick={onClose}
+            >
+              <Image src={restartGame} alt="play again" width="30" />
+            </button>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
 
